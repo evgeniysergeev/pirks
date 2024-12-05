ef3abcc23805df6ce5dc67a2a790357512d2a345
Evgeniy S <evgeniy.sergeev@gmail.com>
2024-12-05 14:04:24 +0300

    [tests] More tests for CircularBuffer

--- tests/common-tests/CircularBufferTest.cpp
+++ tests/common-tests/CircularBufferTest.cpp
@@ -4,7 +4,14 @@
 
 using namespace std::literals;
 
-TEST(CircularBufferEmptyTest, BasicAssertions)
+template<class T>
+class CircularBufferEx : public CircularBuffer<T>
+{
+public:
+    explicit CircularBufferEx(size_t maxElements = 256) : CircularBuffer<T>(maxElements) {};
+};
+
+TEST(CircularBuffer, Initialization)
 {
     CircularBuffer<int> buff;
     EXPECT_TRUE(buff.isEmpty());
@@ -12,4 +19,23 @@ TEST(CircularBufferEmptyTest, BasicAssertions)
     EXPECT_TRUE(buff.isActive());
     EXPECT_EQ(buff.peek(), std::nullopt);
     EXPECT_EQ(buff.peek(100ms), std::nullopt);
+}
+
+TEST(CircularBuffer, BufferStopped)
+{
+    CircularBuffer<int> buff(32);
+    EXPECT_EQ(buff.capacity(), 32);
+
+    buff.stop();
+    EXPECT_TRUE(buff.isEmpty());
+    EXPECT_FALSE(buff.isFull());
+    EXPECT_FALSE(buff.isActive());
+    EXPECT_EQ(buff.capacity(), 32);
+    EXPECT_EQ(buff.peek(), std::nullopt);
+    EXPECT_EQ(buff.peek(100ms), std::nullopt);
+}
+
+TEST(CircularBuffer, PrintEmptyBuff)
+{
+    CircularBufferEx<int> buff(2);
 }
