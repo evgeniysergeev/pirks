f61e693119d0c0ca644c646b43ad33582171e122
Евгений Сергеев <e.sergeev@ideco.ru>
2024-12-20 22:44:20 +0300

    [feature] Added CLI11 command line parser

---
+++ cmake/cli11_setup.cmake
@@ -0,0 +1,3 @@
+# Setup CLI11 command line parameters parser
+add_subdirectory(third-party/CLI11)
+target_link_libraries(default_compiler_flags INTERFACE CLI11::CLI11)
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,9 +3,23 @@
 #include "str_utils.h"
 #include "version.h"
 
-int main()
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv)
 {
+    // Print version information early
     spdlog::info("{} v{} (Platform: {})"sv, PROJECT_NAME, PROJECT_FULL_VERSION, PROJECT_PLATFORM);
+
+    CLI::App args { PROJECT_NAME };
+    argv = args.ensure_utf8(argv);  // new argv memory is held by app
+
+    // TODO: Here your flags / options
+
+    try {
+        args.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return args.exit(e);
+    }
 
     return 0;
 }
