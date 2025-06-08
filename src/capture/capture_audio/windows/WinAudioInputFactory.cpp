cc9c8c7b1e972a129d4380a3d64afcdaa10728d8
Evgeniy S. <evgeniy.sergeev@gmail.com>
2025-06-09 00:31:36 +0300

    [fix] Fixed compilation warnings and errors on Windows

--- src/capture/capture_audio/windows/WinAudioInputFactory.cpp
+++ src/capture/capture_audio/windows/WinAudioInputFactory.cpp
@@ -4,10 +4,10 @@ namespace capture::capture_audio::windows
 {
 
 auto WinAudioInputFactory::create(
-        const std::uint8_t *mapping,
-        int                 channels,
-        std::uint32_t       sample_rate,
-        std::uint32_t       frame_size) -> std::unique_ptr<IAudioInput>
+        [[maybe_unused]] int                 channels,
+        [[maybe_unused]] std::uint32_t       sample_rate,
+        [[maybe_unused]] std::uint32_t       frame_size,
+        [[maybe_unused]] const std::uint8_t *mapping) -> std::unique_ptr<IAudioInput>
 {
     return nullptr;
 }
