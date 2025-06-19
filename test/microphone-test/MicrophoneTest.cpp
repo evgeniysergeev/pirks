b0efab35711c5ee266d600c83c8459808f9d3169
Evgeniy S <evgeniy.sergeev@gmail.com>
2025-06-19 12:31:53 +0300

    [tests] Added test for capturing samples from AudioInput

--- test/microphone-test/MicrophoneTest.cpp
+++ test/microphone-test/MicrophoneTest.cpp
@@ -2,6 +2,8 @@
 
 #include "AudioInputFactory.h"
 
+constexpr auto kSamplesCount = 8;
+
 TEST(AudioInput, CaptureDevice)
 {
     using namespace audio::capture_audio;
@@ -13,4 +15,25 @@ TEST(AudioInput, CaptureDevice)
 
     auto device = audio_input_factory.create(names.at(0), 1, 48000, 96000, nullptr);
     ASSERT_TRUE(device != nullptr);
+}
+
+TEST(AudioInput, GetSamples)
+{
+    using namespace audio;
+    using namespace audio::capture_audio;
+
+    AudioInputFactory audio_input_factory;
+    const auto names = audio_input_factory.getAudioSources();
+
+    std::vector<float> sample_in(1024);
+
+    for (const auto &name: names) {
+        auto device = audio_input_factory.create(name, 2, 48000, 96000, nullptr);
+        ASSERT_TRUE(device != nullptr);
+
+        for (auto i = 0; i < kSamplesCount; i++) {
+            auto result = device->sample(sample_in);
+            ASSERT_TRUE(result == CaptureResult::OK);
+        }
+    }
 }
