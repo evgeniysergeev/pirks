cb27a580afad0dd7353a94a07b56e8f27f9f9acb
Evgeniy S <evgeniy.sergeev@gmail.com>
2025-06-19 12:36:32 +0300

    [docs](audio) Added comments for IAudioInput

--- src/audio/capture_audio/IAudioInput.h
+++ src/audio/capture_audio/IAudioInput.h
@@ -7,12 +7,22 @@
 namespace audio::capture_audio
 {
 
+/**
+ * @brief Interface for capturing audio samples
+ * 
+ */
 class IAudioInput
 {
 public:
     virtual ~IAudioInput() = default;
 
 public:
+    /**
+     * @brief Capture audio sample
+     * 
+     * @param sample_in         Preallocated buffer for sample data
+     * @return CaptureResult    OK if all good
+     */
     virtual auto sample(std::vector<float> &sample_in) -> CaptureResult = 0;
 };
 
