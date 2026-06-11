b0fee8477dc6cc0f6e17bc934bc08079c830b57e
Evgeniy Sergeev <evgeniy.sergeev@gmail.com>
2026-06-11 20:44:11 +0300

    [feature] Added boost WebSockets

--- cmake/beast_setup.cmake
+++ cmake/beast_setup.cmake
@@ -12,6 +12,25 @@ else()
     include(FetchContent)
 
     FetchContent_Declare(
+        boost_headers
+        URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.zip
+    )
+
+    if(POLICY CMP0169)
+        cmake_policy(PUSH)
+        cmake_policy(SET CMP0169 OLD)
+    endif()
+
+    FetchContent_GetProperties(boost_headers)
+    if(NOT boost_headers_POPULATED)
+        FetchContent_Populate(boost_headers)
+    endif()
+
+    if(POLICY CMP0169)
+        cmake_policy(POP)
+    endif()
+
+    FetchContent_Declare(
         beast_v2
         URL https://github.com/boostorg/beast/archive/refs/heads/develop.zip
     )
@@ -19,12 +38,14 @@ else()
     # Beast v2 is header-only; use MakeAvailable but override the target creation
     FetchContent_MakeAvailable(beast_v2)
 
-    set(BOOST_HEADERS_SOURCE_DIR "${beast_v2_SOURCE_DIR}/../boost_headers-src")
+    set(BOOST_HEADERS_SOURCE_DIR "${boost_headers_SOURCE_DIR}")
     if(EXISTS "${BOOST_HEADERS_SOURCE_DIR}/libs")
         file(GLOB BOOST_LIBRARY_INCLUDE_DIRS CONFIGURE_DEPENDS
             "${BOOST_HEADERS_SOURCE_DIR}/libs/*/include"
             "${BOOST_HEADERS_SOURCE_DIR}/libs/*/*/include"
         )
+    else()
+        message(FATAL_ERROR "Boost headers were not found at ${BOOST_HEADERS_SOURCE_DIR}")
     endif()
 
     if(NOT TARGET Boost::asio)
